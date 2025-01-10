f7f5deb920ded0dfa810031d029cec0cd61bb92d
Bryce Langford <156014790+Ambrosius1963@users.noreply.github.com>
2025-01-10 12:33:17 -0700

    Added previous file to new repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview
+
+This is to see if I am still knowledgeable at creating a repository to Github
+
+# Development Environment
+
+I used Python because it was the easiest for me at the time
+
+# Useful Websites
+
+None
---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Hello World in Python to test my connection with GitHub
+def main():
+    print("Hello, World")
+    print("All we have to decide is what to do with the time that is given us.")
+    print(" - Gandalf")
+
+if __name__ == "__main__":
+    main()
