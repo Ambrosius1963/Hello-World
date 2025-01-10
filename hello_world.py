2d5891a401a6ba90cca986e930a780390d02395b
Bryce Langford <bryceslangford@gmail.com>
2025-01-10 12:39:18 -0700

    more format changes

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 # Hello World in Python to test my connection with GitHub
 def main():
-    print("Hello, World")
-    print("All we have to decide is what to do with the time that is given us.")
-    print("Fool of a Took! Throw yourself in next time and rid us of your stupidity!")
-    print(" - Gandalf")
+    print("\nHello, World")
+    print("\"All we have to decide is what to do with the time that is given us.\"")
+    print("\"Fool of a Took! Throw yourself in next time and rid us of your stupidity!\"")
+    print(" - Gandalf\n")
 
 if __name__ == "__main__":
     main()
