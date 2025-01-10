db007baf16bc459c0857f12372d63472e5e6424a
Bryce Langford <bryceslangford@gmail.com>
2025-01-10 12:37:47 -0700

    new Quote

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,7 @@
 def main():
     print("Hello, World")
     print("All we have to decide is what to do with the time that is given us.")
+    print("Fool of a Took! Throw yourself in next time and rid us of your stupidity!")
     print(" - Gandalf")
 
 if __name__ == "__main__":
